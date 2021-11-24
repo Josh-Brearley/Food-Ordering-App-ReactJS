4b9ea009fd72e81ae5849e73658c1452f185e82b
Josh Brearley <brizz@engineer.com>
2021-11-24 19:22:44 +0000

    added extra files.

---
+++ src/components/Meals/MealsSummary.js
@@ -0,0 +1,18 @@
+import classes from "./MealSummary.module.css";
+
+const MealsSummary = () => {
+  return (
+    <section className={classes.summary}>
+      <h2> Great food, straight to your door.</h2>
+      <p>
+        Choose your meal from a wide selection of delicious, fresh ingredients.
+      </p>
+      <p>
+        Each meal is carefully put together to support many diet plans, with the
+        ability to add or subract anything to your order.
+      </p>
+    </section>
+  );
+};
+
+export default MealsSummary;
